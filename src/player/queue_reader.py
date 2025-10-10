64593261ee4504cad2f34cb09c4c870b5c8a900c
Jobin Rezai <jrezai@outlook.com>
2025-10-10 12:45:31 -0400

    Add new files, used for web connectivity.

---
+++ src/player/queue_reader.py
@@ -0,0 +1,41 @@
+"""
+Copyright 2023, 2024 Jobin Rezai
+
+This file is part of LVNAuth.
+
+LVNAuth is free software: you can redistribute it and/or modify it under the terms of
+the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+LVNAuth is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+more details.
+
+You should have received a copy of the GNU General Public License along with
+LVNAuth. If not, see <https://www.gnu.org/licenses/>. 
+"""
+
+import pygubu
+from response_code import ServerResponseReceipt
+
+    
+class QueueMsgReader:
+    """
+    Reads server responses and runs the appropriate methods.
+    
+    By the time the responses reach here, we're no longer in a secondary
+    thread, but in the main GUI thread.
+    """
+    
+    def __init__(self, builder: pygubu.Builder):
+        """
+        Arguments:
+        
+        - builder: for accessing widgets.
+        """
+        self.builder = builder
+        
+    def read_msg(self, msg: ServerResponseReceipt):
+        
+        msg.callback_method(msg)
