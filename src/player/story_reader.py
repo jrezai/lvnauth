39bf5412d63f0ec599844242e9e0f6e8be1dfcaf
Jobin Rezai <jrezai@outlook.com>
2026-05-10 22:50:24 -0400

    Simplify the set volume code

--- src/player/story_reader.py
+++ src/player/story_reader.py
@@ -1827,138 +1827,31 @@ class StoryReader:
 
         def get_volume_from_convenient_value(self, convenient_value: int):
             """
-            Take a value from 0 to 10 and return its float equivalent.
-
+            Take a value from 0 to 100 and return its float equivalent.
+        
             pygame uses 0 to 1.0
             LVNAuth accepts: 0 to 100
             """
-
-            float_values = """0.00
-    0.01
-    0.02
-    0.03
-    0.04
-    0.05
-    0.06
-    0.07
-    0.08
-    0.09
-    0.10
-    0.11
-    0.12
-    0.13
-    0.14
-    0.15
-    0.16
-    0.17
-    0.18
-    0.19
-    0.20
-    0.21
-    0.22
-    0.23
-    0.24
-    0.25
-    0.26
-    0.27
-    0.28
-    0.29
-    0.30
-    0.31
-    0.32
-    0.33
-    0.34
-    0.35
-    0.36
-    0.37
-    0.38
-    0.39
-    0.40
-    0.41
-    0.42
-    0.43
-    0.44
-    0.45
-    0.46
-    0.47
-    0.48
-    0.49
-    0.50
-    0.51
-    0.52
-    0.53
-    0.54
-    0.55
-    0.56
-    0.57
-    0.58
-    0.59
-    0.60
-    0.61
-    0.62
-    0.63
-    0.64
-    0.65
-    0.66
-    0.67
-    0.68
-    0.69
-    0.70
-    0.71
-    0.72
-    0.73
-    0.74
-    0.75
-    0.76
-    0.77
-    0.78
-    0.79
-    0.80
-    0.81
-    0.82
-    0.83
-    0.84
-    0.85
-    0.86
-    0.87
-    0.88
-    0.89
-    0.90
-    0.91
-    0.92
-    0.93
-    0.94
-    0.95
-    0.96
-    0.97
-    0.98
-    0.99
-    1.00"""
-
-            # Key: convenient value (such as 50)
-            # Value: float value (such as 0.50)
-            volume_mapping = {}
-
-            float_lines = float_values.split()
-
-            # Populate volume mapping dictionary
-            for idx, float_value in enumerate(float_lines):
-                volume_mapping[idx] = float_value
-
-            float_value = volume_mapping.get(convenient_value)
-
+        
+            # No value or zero means zero volume.
+            if not convenient_value:
+                float_value = float(0)
+                
+            # More than 100 means volume 1.0 for pygame.
+            elif convenient_value > 100:
+                float_value = float(1)
+            else:
+                # Between 1 and 100
+                
+                # Convert the convenient value to a float
+                # value, between 0.1 and 1.0, so pygame can use it.
+                float_value = convenient_value / 100
+        
             return float_value
 
-        float_volume = get_volume_from_convenient_value(
-            self, convenient_value=volume.volume
-        )
-
-        if not float_volume:
-            return
-
-        # Convert the string value to a float, because pygame will
-        # not accept a string.
-        float_volume = float(float_volume)
+        float_volume =\
+            get_volume_from_convenient_value(self,
+                                             convenient_value=volume.volume)
 
         channel_mapping = {
             "volume_text": audio_player.AudioChannel.TEXT,
