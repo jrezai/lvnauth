b30536da3f5cc399dc680638c31ceb3f46753b07
Jobin Rezai <jrezai@outlook.com>
2026-06-19 16:18:08 -0400

    Set version and update release date

--- src/project_snapshot.py
+++ src/project_snapshot.py
@@ -32,7 +32,7 @@ class ProjectSnapshot:
     EXACT_EDITOR_VERSION = "1.51"
     
     # Used in the Startup window.
-    RELEASE_DATE = "June 14, 2026"
+    RELEASE_DATE = "June 19, 2026"
 
     # Default the story window size to 640x480
     story_window_size = (640, 480)
